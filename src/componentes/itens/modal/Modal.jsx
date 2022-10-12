375e4cac22cd9d864f413c0dd7d77378d8ee4393
ERICK <erick_honorio20@hotmail.com>
2022-10-12 20:51:11 -0300

    Fix editar produto

--- src/componentes/itens/modal/Modal.jsx
+++ src/componentes/itens/modal/Modal.jsx
@@ -68,7 +68,7 @@ export default function Modal(props){
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id={`modal${props.produto.id}Label`}>Adicionar novo produto</h1>
+                        <h1 class="modal-title fs-5" id={`modal${props.produto.id}Label`}>Editar produto</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
